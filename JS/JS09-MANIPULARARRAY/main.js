a5a32b9e89d9489ff16cd673a699a88a7f13483d
Vanolli96 <carlos.vanolli@gmail.com>
2024-11-08 20:51:38 -0300

    Aula de hoje 08/11

--- JS/JS09-MANIPULARARRAY/main.js
+++ JS/JS09-MANIPULARARRAY/main.js
@@ -1,7 +1,7 @@
 // SuperSimpleDev
 
 //ARRAY
-let frutas = [ "Manga", "Tomate", "Açai", "Pessego", "Limao", "Melancia", "Goiaba", "Abacaxi" ]
+// let frutas = [ "Manga", "Tomate", "Açai", "Pessego", "Limao", "Melancia", "Goiaba", "Abacaxi" ]
 
 // for( i=0; i <= frutas.length ; i++){
 //     console.log(frutas[i]); // conteudo da posição 
@@ -10,10 +10,10 @@ let frutas = [ "Manga", "Tomate", "Açai", "Pessego", "Limao", "Melancia", "Goia
 
 frutas.forEach(Percorrer)
 ///                conteudo, indice  
-function Percorrer(indice, valor){
-    console.log(valor); 
-    console.log(indice)
-}
+// function Percorrer(indice, valor){
+//     console.log(valor); 
+//     console.log(indice)
+// }
 
 
 
@@ -27,3 +27,56 @@ function Percorrer(indice, valor){
 
 //let JsonAluno = JSON.stringfy(aluno)
 //console.log()
+
+
+
+// array de valores aleatorios com 06 posições > numeros 
+
+let numeros = [2 , 6, 8, 28, 33, 63]
+
+// trazer um novo array atualizado com raiz quadrada de cada numero do array
+
+numeros.map(Math.sqrt())
+
+// Trazer um novo array atualizado com a raiz quadrado de cada numero do array 
+
+let numeroDois = numeros.map(Math.sqrt)
+let maiorNumero = numeros.map(Math.max)
+let menorNumero = Math.min.apply(Math, numeros)
+
+console.log(numeros)
+console.log(numerosDois)
+console.log(menorNumero)
+
+let filtraNumero = numeros.filter((valor) => {
+    return valor >=
+
+})
+
+console.log(filtraNumero)
+
+console.clear()
+
+let tarefa = [
+    {
+        tarefa: "Mimir"
+        isFeita: true
+    }
+    {
+        tarefa: "Limpar"
+        isFeita: false
+    }
+    {
+        tarefa: "Torcer"
+        isFeita: true
+    }
+]
+
+// console.log(tarefa.find((item) =>{
+//     return item.isFeita == true
+
+// }))
+
+let retornaTrue = tarefa.find((item) => {
+    return item.isFeira == true
+} )
