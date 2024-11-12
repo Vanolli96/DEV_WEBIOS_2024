26c7ef43cf816b5e56334247fd3cdf31767d4d8d
Carlos Alberto <carlos.vanolli@gmail.com>
2024-11-12 00:24:00 -0300

    Add files via upload
    atv 01

--- JS/JS07-Exercicio/main.js
+++ JS/JS07-Exercicio/main.js
@@ -1,48 +1,9 @@
+let numeros = [17, 43, 8, 4, 97, 56, 29, 95];
 
-// Crie um Arrays de Objetos chamado aluno com as informações abaixo:
-
-let aluno = [
-    {
-        nome: 'Ana',
-        idade: 17,
-        nota: 8,
-        ano: '2B'
-    },
-    {
-        nome: 'Bruno',
-        idade: 12,
-        nota: 6,
-        ano: '2C'
-    },
-    {
-        nome: 'Veronica',
-        idade: 16,
-        nota: 9,
-        ano: '2C',
-    },
-    {
-        nome: 'Marta',
-        idade: 15,
-        nota: 5,
-        ano: '3C',
-    },
-    {
-        nome: 'Brenno',
-        idade: 19,
-        nota: 6,
-        ano: '3C',
-    },
-    {
-        nome: 'Maria',
-        idade: 14,
-        nota: 4,
-        ano: '1F',
-    }
-]
-
-console.log(aluno) // Retorne todo o array de objetos
-console.log("Aluno Brunno" , aluno[1])// Retorne todas as informações do aluno Brunno
-console.log('Nota de Maria: ' , aluno[5].nota ) 
-// Retorne apenas a nota da Maria
-
-console.log("---JSON---" + JSON.stringify(aluno)) // Retorno todo o array de objeto em formato JSON
+numeros.forEach(numero => {
+  if (numero % 2 === 0) {
+    console.log(`O número ${numero} é par.`);
+  } else {
+    console.log(`O número ${numero} é ímpar.`);
+  }
+});
