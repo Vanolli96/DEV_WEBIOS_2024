783579fa4e641f576b513bbca313fd482515a5f3
Vanolli96 <carlos.vanolli@gmail.com>
2024-11-14 21:22:31 -0300

    tcc14.11

---
+++ JS/JS12+1-DOM-03/main.js
@@ -0,0 +1,17 @@
+let button = document.querySelector('button')
+
+let = olhaNumero = true
+if (olhaNumero === true) {
+    botao.style.color = "orange"
+    botao.style.backgroundColor = "green"
+    botao.style.borderRadius = "5px"
+    botao.innerHTML = "Cadastrar"
+
+} else {
+    botao.style.color = "red"
+
+}
+
+let paragrafo = document.querySelector('#paragrafo')
+paragrafo.style.backgroundImage = 'Linear-gradient(greed,white)'
+paragrafo.style.color = "white"
